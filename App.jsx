a6c5a9e50910584ce8b36af4a431f8d8e1c89001
10MIBI <rajasalame@gmail.com>
2025-10-22 15:17:15 +0200

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,1 +1,16 @@
-// React frontend entry with Stripe Checkout button placeholder
+import React from 'react';
+import ProjectForm from './components/ProjectForm';
+import ProjectList from './components/ProjectList';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>🎬 O-MDB - Gestion de Projets</h1>
+      <ProjectForm />
+      <hr />
+      <ProjectList />
+    </div>
+  );
+}
+
+export default App;
